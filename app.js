e87d2a1ef3f716e874cd55c5aaf2a17ae4f21f74
Muhammad Ovais Naeem <monaeem118@gmail.com>
2023-11-05 23:10:02 -0600

    Passport JWT

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-const session = require("express-session");
-const FileStore = require("session-file-store")(session);
 const passport = require("passport");
-const authenticate = require("./authenticate");
+const config = require("./config");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -33,46 +30,19 @@ connect.then(
 var app = express();
 
 // view engine setup
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser("12345-67890-09876-54321"));
-
-app.use(
-  session({
-    name: "session-id",
-    secret: "12345-67890-09876-54321",
-    saveUninitialized: false,
-    resave: false,
-    store: new FileStore(),
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-
-function auth(req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-    const err = new Error("You are not authenticated!");
-    err.status = 401;
-    return next(err);
-  } else {
-    return next();
-  }
-}
-
-app.use(auth);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser("12345-67890-09876-54321"));
 
-app.use(cookieParser());
+app.use(passport.initialize());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/campsites", campsiteRouter);
